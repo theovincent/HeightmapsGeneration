33f50ece9429d9d014ac10547f1d19457a0ed019
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2020-03-14 21:53:33 +0100

    Second transfer Front end

---
+++ static/script.js
@@ -0,0 +1,12 @@
+/* gestion dynamique sliders */
+let move_slider1 = document.querySelector('input#slider1')
+let move_slider2 = document.querySelector('input#slider2')
+
+move_slider1.addEventListener('input', function (){document.getElementById("h_max").innerHTML = this.value });
+move_slider2.addEventListener('input', function(){document.getElementById("h_mean").innerHTML = this.value });
+
+/* affichage dynamique de l'image 3D */
+function viewImage(id,path_image) {
+      var address='<img src={}>';
+      document.getElementById(id).innerHTML=address.replace("{}",path_image);
+}
